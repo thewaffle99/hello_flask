818b497827700a05fd6f8ca0e182635c1be97a01
Rene Marino <renemarino93@gmail.com>
2022-03-15 16:32:39 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+
+@app.route('/success')
+def success():
+    return "success"
+
+
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
